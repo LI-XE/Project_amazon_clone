767e3a8ec46d2785a9683617d087b638dbc39ae7
LI_XE <hyangeun312@gmail.com>
2022-11-01 01:15:08 -0500

    updated

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose
-  .connect(
-    MONGODB_URL
-    //    {
-    //   useNewUrlParser: true,
-    //   // useUnifiedTopology: true,
-    // }
-  )
-  .then(() => console.log(`Established a connection to the  database`))
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Connected the  database`))
   .catch((err) =>
     console.log(`Something went wrong when connecting to the  database`, err)
   );
