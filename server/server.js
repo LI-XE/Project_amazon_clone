81a3a350e76d5eb73f88cd37a1ff6b82df894c55
LI_XE <hyangeun312@gmail.com>
2022-11-04 23:50:01 -0500

    updated

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 
 // const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/amazon_clone/build")));
-app.get("*", (req, res) =>
+app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "/amazon_clone/build/index.html"))
 );
 
