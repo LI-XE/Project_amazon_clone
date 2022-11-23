1bd04070b23ab51726810e9b209ebd35a9088b4f
LI_XE <hyangeun312@gmail.com>
2022-11-23 01:28:58 -0600

    updated

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose
-  .connect("mongodb+srv://" + MONGODB_URL, {
+  .connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
