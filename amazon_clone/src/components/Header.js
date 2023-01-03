ff3cb41bfdc0c92b22c440bd018efbb960b90c05
LI_XE <hyangeun312@gmail.com>
2023-01-03 00:30:45 -0600

    added price, rating filter

--- amazon_clone/src/components/Header.js
+++ amazon_clone/src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
